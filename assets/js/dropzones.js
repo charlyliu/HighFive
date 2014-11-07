caf1e7e46b09453d9a947e96ca0a98da76ec07e5
Charly Liu Chou <charly.liu@schibsted.es>
2014-11-07 13:24:21 +0100

    products with titles

--- assets/js/dropzones.js
+++ assets/js/dropzones.js
@@ -85,8 +85,9 @@
             })
             .on('drop', function (event) {
                 // removeClass(event.target, '-drop-over');
+
+                jQuery("#drop-zone-sidebar ul").append('<li><div id="dragable-sidebar" class="draggable js-drag"><img width=160 src='+event.relatedTarget.childNodes[1].childNodes[1].src+'></img><h3>'+ event.relatedTarget.childNodes[1].childNodes[3].childNodes[1].textContent +'</h3></div></li>');
                 event.relatedTarget.remove()
-                jQuery("#drop-zone-sidebar ul").append('<li><div id="dragable-sidebar" class="draggable js-drag"><img width=80 src="http://www.ikea.com/PIAimages/20315_PE105482_S5.JPG"></img></div></li>');
             });
     }
 
